0cfa3bd0818e70b4eb78473ab5d76582d9a24d7e
Aaron Zemetres <azemetre@gmail.com>
2017-10-26 18:54:43 -0400

    tests: cleaned up code, lowered sleep timer

--- tests/conversions.js
+++ tests/conversions.js
@@ -4,59 +4,46 @@ const expect = chai.expect;
 
 chai.use(require('chai-as-promised'));
 
-describe('Simple Currency Conversion - Chrome', function() {
+describe('Simple Currency Conversion - Chrome', function () {
   this.timeout(50000);
 
   let driver = new webdriver.Builder()
-      .forBrowser('chrome')
-      .build();
+    .forBrowser('chrome')
+    .build();
 
-  before(function() {
-    
+  before(function () {
     driver.get('http://aaronzemetres.me/portfolio/brightcove-assignment/');
-
     return driver.manage().timeouts().implicitlyWait(5000);
   });
 
-  after(function() {
+  after(function () {
     return driver.quit();
   });
 
   describe('verify currency hasn\'t been converted', function () {
-
-
     it('Exchange rate equals ###.###', function () {
-
       driver.sleep(2500);
       let exchangeTag = driver.findElement({
         css: 'div.container p a#exchangeRate'
       }).getText();
 
       return expect(exchangeTag).to.eventually.equal('###.###');
-
     });
   });
 
   describe('would like to click the convert button', function () {
-    
     it('verify currency has converted', function () {
-
       driver.findElement({
         css: 'div.container button#fixerButton'
       }).click();
 
-      driver.sleep(2500);
+      driver.sleep(1000);
 
       let exchangeTag = driver.findElement({
         css: 'div.container p a#exchangeRate'
       }).getText();
 
       return expect(exchangeTag).to.eventually.not.equal('###.###');
-
     });
   });
-
-
-
-
 });
